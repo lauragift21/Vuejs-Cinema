861084843cfb72cc1fe74e68c085ef6e54872699
egwuenu gift <laurandidi21@gmail.com>
2017-10-26 07:38:49 +0100

    created ne component for handling movie filters

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -1,37 +1,38 @@
 <template>
 	<div id='movie-list'>
-		<div v-for="movie in filteredMovies" class="movie"> {{ movie.title }} </div>
+		<movie-item v-for="movie in  filteredMovies"  :movie="movie.movie"></movie-item>
 	</div>
 </template>
 <script>
 import genres from '../util/genres.js';
+import MovieItem from './MovieItem.vue';
 	export default {
-			data() {
-				return {
-					movies: [
-						{title: 'Homecoming', genre: genres.CRIME},
-						{title: 'Kingsmen', genre: genres.ANIMATION},
-						{title: 'Gentleman', genre: genres.ANIMATION},
-						{title: 'Geostorm', genre:genres.DRAMA}
-					]	
+		props: ['genre', 'time', 'movies'],
+		methods: {
+			moviePassesGenreFilter(movie) {
+				if (!this.genre.length) {
+					return true;
+				} else {
+					let movieGenre = movie.movie.Genre.split(", ");
+					let matched = true;
+					this.genre.forEach(genre => {
+						if (movieGenre.indexOf(genre) === -1) {
+							matched = false;
+						}
+					});
+					return matched;
 				}
-			},
-		props: ['genre', 'time'],
-		// methods: {
-		// 	moviePassesGenreFilter(movie) {
-		// 	if (this.genre.length) {
-		// 	return true;
-		// } else {
-		// 	return this.genre.find(genre => movie.genre === genre);
-		// }
-
-		// 	}
-		// },
+			}
+		},
 		computed: {
 			filteredMovies() {
-				return this.movies();
+				return this.movies.filter(this.moviePassesGenreFilter);
 			}
+		},
+		components: {
+			MovieItem
 		}
+
 		
 
 	}
